0c51584b437d0cdffc1e079463beb8b0e808d112
Anthony Russell <arussell@apterainc.com>
2019-07-17 12:17:22 -0400

    Add project files.

---
+++ DemoBasePageBug/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace DemoBasePageBug
+{
+    public sealed partial class MainPage : TestPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            this.ShowNotification();
+        }
+    }
+}
---
+++ DemoBasePageBug/TestPage.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Toolkit.Uwp.UI.Controls;
+using Windows.UI.Xaml.Controls;
+
+namespace DemoBasePageBug
+{
+    public class TestPage : Page
+    {
+        private AppBar appbar;
+        private InAppNotification note;
+
+        public TestPage()
+        {
+            this.DefaultStyleKey = typeof(TestPage);
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            appbar = GetTemplateChild("appbar") as AppBar;
+            note = GetTemplateChild("note") as InAppNotification;
+        }
+
+        public void ShowNotification()
+        {
+            if (appbar != null)
+                appbar.IsOpen = true;
+            note?.Show();
+        }
+    }
+}
